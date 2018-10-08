711e1c0b1d1ccd9bdbe7f3b6611825ccfb857b7c
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-10-08 14:35:37 +0700

    Config multiple origin socketIO

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const app = express()
 const server = require('http').Server(app)
 var io = require('socket.io')(server, { origins: allowedOrigins})
 
-var allowedOrigins = "https://socialblogjt.netlify.com"
+var allowedOrigins = "https://socialblogjt.netlify.com/* https://socialblogjt.netlify.com"
 
 module.exports = {express, app, server, io}
