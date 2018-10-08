8df3ae800bab8f839633cd98eb5aa33ccf8353a9
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-10-08 14:15:27 +0700

    fix export sio_sever

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const app = express()
 const server = require('http').Server(app)
 var io = require('socket.io')(server)
 
-var allowedOrigins = "http://localhost:* https://socialblogjt.netlify.com";
+var allowedOrigins = "https://socialblogjt.netlify.com";
 var sio_server = io(server, {
     origins: allowedOrigins
 })
 
-module.exports = {express, app, server, io, sio_server}
+module.exports = {express, app, server, io}
