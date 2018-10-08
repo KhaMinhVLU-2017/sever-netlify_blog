e2c687543ba3e23f3904ed112156d15f954fde51
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-10-08 14:10:51 +0700

    config socketIO allow origin client

--- config.js
+++ config.js
@@ -3,5 +3,9 @@ const app = express()
 const server = require('http').Server(app)
 var io = require('socket.io')(server)
 
+var allowedOrigins = "http://localhost:* https://socialblogjt.netlify.com";
+var sio_server = io(server, {
+    origins: allowedOrigins
+})
 
-module.exports = {express, app, server, io}
+module.exports = {express, app, server, io, sio_server}
