563803c6f8f4c762485694ec762f54de6e6c5049
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-10-08 14:18:00 +0700

    Config complete sockio allow origins

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-var io = require('socket.io')(server)
+var io = require('socket.io')(server, { origins: allowedOrigins})
 
-var allowedOrigins = "https://socialblogjt.netlify.com";
-var sio_server = io(server, {
-    origins: allowedOrigins
-})
+var allowedOrigins = "https://socialblogjt.netlify.com"
 
 module.exports = {express, app, server, io}
