940d3f375fd3d8aa426a1f09ff4386b88ba0dd8c
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-10-09 19:57:29 +0700

    only config with index auithor 0

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 var io = require('socket.io')(server, { origins: allowedOrigins})
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 var allowedOrigins = "https://socialblogjt.netlify.com/* https://socialblogjt.netlify.com"
 
 module.exports = {express, app, server, io}
--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
 const postController = require('./api/controller/postController')
 const loginCon = require('./api/controller/loginController')
 const express = require('express')
