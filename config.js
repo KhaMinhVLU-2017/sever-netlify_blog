b4a89d6a3ead68456a8c1796fa21822365e7fe93
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-10-16 22:37:52 +0700

    Change config IO

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-var io = require('socket.io')(server, { origins: allowedOrigins})
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
-var allowedOrigins = "https://socialblogjt.netlify.com/* https://socialblogjt.netlify.com"
+var io = require('socket.io')(server)
 
 module.exports = {express, app, server, io}
