2ddbd67963ae6ebebf6cac5d64679fa070ec7439
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-10-25 23:36:59 +0700

    Change config socketio

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-var io = require('socket.io')(server)
+var io = require('socket.io')({path: '/blogserver', server})
 
 module.exports = {express, app, server, io}
